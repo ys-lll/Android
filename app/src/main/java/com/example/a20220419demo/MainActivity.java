fd22e81e4b29d674a4edb11f7be4757e699f4589
ys-lll <18242100197@163.com>
2022-05-07 16:07:04 +0800

    nineth homework

--- app/src/main/java/com/example/a20220419demo/MainActivity.java
+++ app/src/main/java/com/example/a20220419demo/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.a20220419demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +34,38 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(MainActivity.this, "账号密码错误", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+//        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+//        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+//        //网络状态
+//        int netType = networkInfo.getType();
+//        //假设预置的网络状态为有网络
+//        if (netType == 0){
+//            Toast.makeText(MainActivity.this,"此时有数据连接，与预设状态相同",Toast.LENGTH_SHORT).show();
+//        }else{
+//            Toast.makeText(MainActivity.this,"此时不是数据连接，与预设状态不相同",Toast.LENGTH_SHORT).show();
+//        }
+
+        final ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+        activityManager.getMemoryInfo(memoryInfo);
+        //剩余内存
+        long freeMemory = memoryInfo.availMem;
+        //内存的总量
+        long totalMemory = memoryInfo.totalMem;
+        String memory = "开机完成时的剩余内存为" +  String.valueOf(memoryInfo.availMem / (1024 * 1024)) + "MB";
+
+        Button button1 = findViewById(R.id.bbbb);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,memory,Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+                intent.putExtra("memory",memory);
+                intent.setClass(MainActivity.this,NinethActivity.class);
+                startActivity(intent);
             }
         });
 
